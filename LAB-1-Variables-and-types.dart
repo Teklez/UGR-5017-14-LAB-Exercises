c065173e01015bea3c2597157845739dc0b31cac
Teklez <zemenumekiria@gmail.com>
2024-03-05 16:31:53 +0300

    LAB-1-Variables-and-types

---
+++ LAB-1-Variables-and-types.dart
@@ -0,0 +1,13 @@
+void main() {
+  // Exercise 1
+  int a = 5;
+  int b = 10;
+  int c = 15;
+  int sum = a + b + c;
+  print('Sum of a, b, and c: $sum');
+
+  // Exercise 2
+  String str = "zemenu";
+  int length = str.length;
+  print('Length of the string: $length');
+}
