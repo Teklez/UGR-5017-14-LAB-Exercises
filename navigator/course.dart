9193f6294c9acd6bec256ed0a78906e59837bda5
zemen <zemenumekiria@gmail.com>
2024-04-08 16:33:28 +0300

    navigator exercise

--- navigator/course.dart
+++ navigator/course.dart
@@ -15,24 +15,3 @@ class Course {
 // course list screeen
 
 
-class CoursesListScreen extends StatelessWidget {
-  const CoursesListScreen({required this.courses,required this.onTapped})
-  final List<Course> courses;
-  final void Function(Course course) onTapped;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar:AppBar(title: const Text('Courses List'),),
-      body: ListView(
-        children: [
-          for (var course in courses)
-          ListTile(
-            title: Text(course.title),
-            subtitle: Text(course.code),
-            onTap: () => onTapped(course),
-          ),
-        ],),
-    );
-  }
-}
